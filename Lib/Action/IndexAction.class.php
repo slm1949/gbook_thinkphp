5300c6ff984fe4b2ae8a9f5443079a338eb50150
slm1949 <slm1949@163.com>
2013-01-07 00:20:54 +0800

    新增登陆状态判断，为实现管理员后台回复提供条件 #7

--- Lib/Action/IndexAction.class.php
+++ Lib/Action/IndexAction.class.php
@@ -4,6 +4,27 @@ class IndexAction extends Action {
 	$info=M('gbook');
         $data=$info->select();
         $this->assign('data',$data);
-        $this->display();//渲染，调用模板引擎12
+        session_start();
+        if(empty($_SESSION['name'])){
+           $this->display();
+         }
+        else{
+           $this->admin=$_SESSION;
+           $this->display('index_admin');
+        }
      }
+    public function admin_back(){
+         $Form=M('user');
+         $name=$_POST['name'];
+         $password=$_POST['password'];
+         if($password==$Form->where("name='$name'")->getField('password')){
+          $this->message='用户权限验证成功';  
+          $_SESSION['name']=$name;
+          $this->display('__APP__/Form/go');
+         }
+         else{
+           $this->error('管理员名或密码错误');
+         }
+                 
+    }
 }
